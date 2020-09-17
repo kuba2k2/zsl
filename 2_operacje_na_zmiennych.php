8ffe43624a86b968de54273778f6440ab151e311
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2020-09-17 19:27:40 +0200

    php

--- 2_operacje_na_zmiennych.php
+++ 2_operacje_na_zmiennych.php
@@ -6,3 +6,23 @@ echo  PHP_VERSION;
 $x = 10;
 $y = 20;
 echo $x <=> $y;
+
+if ($x==$y) {
+echo 'Zmienne są równe';
+}
+else {
+echo 'Zmienne są różne';
+}
+
+$x = 2;
+echo $x++; // 2
+echo ++$x; // 4
+echo $x; // 4
+$y = $x++;
+echo $y; // 4
+$y = ++$x; // 6
+echo $y; // 6
+echo ++$y; // 7
+
+
+
